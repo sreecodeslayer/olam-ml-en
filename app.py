34db773de2616b13d83c2791472ff6ccb4271489
Sreenadh T C <sreenadh@datahut.co>
2017-03-16 17:24:25 +0530

    Update app to auto import/update CSV on run

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask,jsonify,request
 from pymongo import MongoClient
 
+
+import sys,os,json
+import pandas as pd
+
 app = Flask(__name__)
 db = MongoClient()
 olam = db['olam']['olam-enml']
+APP_ROOT = os.path.dirname(__file__)
+def import_csv_to_db():
+	file_res = os.path.join(APP_ROOT, 'olam-enml.csv')
+	print file_res
+	csv_data = pd.read_csv(file_res,sep='\t')
+	json_data = json.loads(csv_data.to_json(orient='records'))
+	olam.remove()
+	olam.insert(json_data)
 
 @app.route('/')
 def index():
@@ -20,4 +32,5 @@ def search():
 	return jsonify({'result':result})
 
 if __name__ == '__main__':
+	import_csv_to_db()
 	app.run(debug=True, threaded=False, host='127.0.0.1',port=1122)
